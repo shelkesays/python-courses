9629ff9c53dbb76540fea83ae606a54de06df7b7
srahul07 <srahul07@gmail.com>
2014-09-04 23:15:24 +0530

    Exercise 7: drill 1

--- ex7.py
+++ ex7.py
@@ -1,21 +1,38 @@
+# print string
 print "Mary had a little lamb."
+# print string
 print "Its fleece was white as %s." % 'snow'
+# print string
 print "And everywhere that Mary went."
+# print string 10 times
 print "." * 10 # what'd that do?
 
+# define variable end1
 end1 = "C"
+# define variable end2
 end2 = "h"
+# define variable end3
 end3 = "e"
+# define variable end4
 end4 = "e"
+# define variable end5
 end5 = "s"
+# define variable end6
 end6 = "e"
+# define variable end7
 end7 = "B"
+# define variable end8
 end8 = "u"
+# define variable end9
 end9 = "r"
+# define variable end10
 end10 = "g"
+# define variable end11
 end11 = "e"
+# define variable end12
 end12 = "r"
 
 # watch that comma a the end. try removing it to see what happens
+# print both print lines with spaces in between workds, replace by comma
 print end1 + end2 + end3 + end4 + end5 + end6,
 print end7 + end8 + end9 + end10 + end11 + end12
