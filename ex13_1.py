d460077bad9a47cc6c7d34a01fd5ea0b5bdac06b
srahul07 <srahul07@gmail.com>
2014-09-05 23:35:49 +0530

    Exercise 13: drill 1, 2

---
+++ ex13_1.py
@@ -0,0 +1,5 @@
+from sys import argv
+
+script, one = argv
+
+print "Script: %r\nOne: %r" % (script, one)
---
+++ ex13_2.py
@@ -0,0 +1,5 @@
+from sys import argv
+
+script, one, two, three, four, five, six = argv
+
+print script, one, two, three, four, five, six
