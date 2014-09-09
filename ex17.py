f0c6bfa97b1476c27e49525936ce9a52e587e416
srahul07 <srahul07@gmail.com>
2014-09-09 23:37:08 +0530

    Exercise 17: drill 2

--- ex17.py
+++ ex17.py
@@ -3,22 +3,23 @@ from os.path import exists
 
 script, from_file, to_file = argv
 
-print "Copying from %s to %s" % (from_file, to_file)
+#print "Copying from %s to %s" % (from_file, to_file)
 
 # we could do these two on one line too, how?
 in_file = open(from_file)
-indata = in_file.read()
+#indata = in_file.read()
 
-print "The input file is %d bytes long" % len(indata)
+#print "The input file is %d bytes long" % len(indata)
 
-print "Does the output file exist? %r" % exists(to_file)
-print "Ready, hit RETURN to continue, CTRL-C to abort."
-raw_input()
+#print "Does the output file exist? %r" % exists(to_file)
+#print "Ready, hit RETURN to continue, CTRL-C to abort."
+#raw_input()
 
 out_file = open(to_file, 'w')
-out_file.write(indata)
+#out_file.write(indata)
+out_file.write(in_file.read())
 
-print "Alright, all done."
+#print "Alright, all done."
 
 out_file.close()
 in_file.close()
