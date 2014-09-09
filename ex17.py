3a072ba9954833d5ee0743b901e6ab020119136a
srahul07 <srahul07@gmail.com>
2014-09-09 23:48:38 +0530

    Exercise 17: drill 3

--- ex17.py
+++ ex17.py
@@ -6,7 +6,7 @@ script, from_file, to_file = argv
 #print "Copying from %s to %s" % (from_file, to_file)
 
 # we could do these two on one line too, how?
-in_file = open(from_file)
+#in_file = open(from_file)
 #indata = in_file.read()
 
 #print "The input file is %d bytes long" % len(indata)
@@ -15,11 +15,13 @@ in_file = open(from_file)
 #print "Ready, hit RETURN to continue, CTRL-C to abort."
 #raw_input()
 
-out_file = open(to_file, 'w')
+#out_file = open(to_file, 'w')
 #out_file.write(indata)
-out_file.write(in_file.read())
+#out_file.write(open(from_file).read())
 
 #print "Alright, all done."
 
-out_file.close()
-in_file.close()
+#out_file.close()
+#in_file.close()
+
+open(to_file, 'w').write(open(from_file).read())
