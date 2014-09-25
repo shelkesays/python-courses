449234fe3be9be3505fb5387c43f203f4f508bca
= <srahul07@gmail.com>
2014-09-25 12:14:20 +0530

    Added puzzle solutions

--- ex21.py
+++ ex21.py
@@ -14,8 +14,12 @@ def divide(a, b):
     print "DIVIDING %d / %d" % (a, b)
     return a / b
 
+def puzzle(a, b, c, d):
+    print "Solving puzzle with %d, %d, %d, %d" % (a, b, c, d)
+    return (a + (b - (c * (d / 2))))
 
-print "Let's do some math with just functoins!"
+
+print "Let's do some math with just functions!"
 
 age = add(30, 5)
 height = subtract(78, 4)
@@ -31,3 +35,6 @@ print "Here is a puzzle."
 what = add(age, subtract(height, multiply(weight, divide(iq, 2))))
 
 print "That becomes: ", what, "Can you do it by hand?"
+
+result = puzzle(age, height, weight, iq)
+print result
