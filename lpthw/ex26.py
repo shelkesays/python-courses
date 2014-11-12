72f5a2e8e4ce2d3540f843324bb78555cc81133b
= <srahul07@gmail.com>
2014-11-12 12:27:09 +0530

    fixing error: Traceback (most recent call last): File "ex26.py", line 87, in <module> sorted_words = ex25.sort_words(words) NameError: name ex25 is not defined

--- lpthw/ex26.py
+++ lpthw/ex26.py
@@ -84,7 +84,7 @@ print "We'd have %d beans, %d jars, and %d crabapples." % secret_formula(start_p
 sentence = "All god\tthings come to those who weight."
 
 words = break_words(sentence)
-sorted_words = ex25.sort_words(words)
+sorted_words = sort_words(words)
 
 print_first_word(words)
 print_last_word(words)
