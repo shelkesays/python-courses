9ebd88209018edd1221f97fab67f8901b5e1e05a
= <srahul07@gmail.com>
2014-11-12 14:10:01 +0530

    Exercise 32: study drill 2

--- lpthw/ex32.py
+++ lpthw/ex32.py
@@ -16,13 +16,13 @@ for i in change:
     print "I got %r" % i
 
 # we can also build lists, first start with an empty one
-elements = []
-
+#elements = []
+elements = range(0, 6)
 # then use the range function to do 0 to 5 counts
-for i in range(0, 6):
-    print "Adding %d to the list." % i
+#for i in range(0, 6):
+#    print "Adding %d to the list." % i
     # append is a function that lists understand
-    elements.append(i)
+#    elements.append(i)
 
 # now we can print them out too
 for i in elements:
