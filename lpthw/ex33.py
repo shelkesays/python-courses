8ef01d10706d5c983e582694ab526ff37184fc54
= <srahul07@gmail.com>
2014-11-13 11:34:10 +0530

    Exercise 33: Drill 3

--- lpthw/ex33.py
+++ lpthw/ex33.py
@@ -1,17 +1,17 @@
-def whileTest(maxvalue):
+def whileTest(maxvalue, incrementor):
     i = 0
     numbers = []
     while i < maxvalue:
         print "At the top i is %d" % i
         numbers.append(i)
 
-        i = i + 1
+        i = i + incrementor
         print "Numbers now: ", numbers
         print "At the bottom i is %d" % i
 
     return numbers
 
-numbers = whileTest(int(raw_input("Enter loopcounter: ")))
+numbers = whileTest(int(raw_input("Enter loop counter: ")), int(raw_input("Enter Incrementor: ")))
 print "The numbers: "
 
 for num in numbers:
