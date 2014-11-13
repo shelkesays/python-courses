6350a556273674f3fc6bb56eee797098f427e9ed
= <srahul07@gmail.com>
2014-11-13 11:32:32 +0530

    Exercise 33: Drill 1, 2

--- lpthw/ex33.py
+++ lpthw/ex33.py
@@ -1,15 +1,17 @@
-i = 0
-numbers = []
+def whileTest(maxvalue):
+    i = 0
+    numbers = []
+    while i < maxvalue:
+        print "At the top i is %d" % i
+        numbers.append(i)
 
-while i < 6:
-    print "At the top i is %d" % i
-    numbers.append(i)
+        i = i + 1
+        print "Numbers now: ", numbers
+        print "At the bottom i is %d" % i
 
-    i = i + 1
-    print "Numbers now: ", numbers
-    print "At the bottom i is %d" % i
+    return numbers
 
-
+numbers = whileTest(int(raw_input("Enter loopcounter: ")))
 print "The numbers: "
 
 for num in numbers:
