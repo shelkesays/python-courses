40943db11e338467d876be8a6efaaebab4fec0d8
= <srahul07@gmail.com>
2014-11-16 17:57:30 +0530

    Exercise 42: with comments

--- lpthw/ex42.py
+++ lpthw/ex42.py
@@ -3,54 +3,55 @@ class Animal(object):
     pass
 
 
-## ??
+## Dog is-a Animal
 class Dog(Animal):
 
     def __init__(self, name):
-        ## ??
+        ## Dog has-a name
         self.name = name
 
 
-## ??
+## Cat is-a Animal
 class Cat(Animal):
 
     def __init__(self, name):
-        ## ??
+        ## Cat has-a name
         self.name = name
 
 
-## ??
+## Person is-a object
 class Person(object):
 
     def __init__(self, name):
-        ## ??
+        ## Perosn has-a name
         self.name = name
 
         ## Person has-a pet of some kind
         self.pet = None
 
 
-## ??
+## Employee is-a Person
 class Employee(Person):
 
     def __init__(self, name, salary):
         ## ?? hmm what is this strange magic?
+        ## Initialize Person which has-a name
         super(Employee, self).__init__(name)
-        ## ??
+        ## Employee has-a salary
         self.salary = salary
 
 
-## ??
+## Fish is-a object
 class Fish(object):
     pass
 
 
-## ??
+## Salmon is-a Fish
 class Salmon(Fish):
     pass
 
 
-## ??
+## Halibut is-a Fish
 class Halibut(Fish):
     pass
 
@@ -58,26 +59,26 @@ class Halibut(Fish):
 ## rover is-a Dog
 rover = Dog("Rover")
 
-## ??
+## satan is-a Cat
 satan = Cat("Satan")
 
-## ??
+## mary is-a Person
 mary = Person("Mary")
 
-## ??
+## mary has-a pet satan
 mary.pet = satan
 
-## ??
+## frank is-a employee with salary 120000
 frank = Employee("Frank", 120000)
 
-## ??
+## frank has-a pet rover
 frank.pet = rover
 
-## ??
+## flipper is-a fish
 flipper = Fish()
 
-## ??
+## crouse is-a Salmon
 crouse = Salmon()
 
-## ??
+## harry is-a Halibut
 harry = Halibut()
