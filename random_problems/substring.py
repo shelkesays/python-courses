2f6db940df43ff327444613a046142548eab948a
Rahul Shelke <srahul07@gmail.com>
2016-05-28 20:59:27 +0530

    Updating code to check for ends with

--- random_problems/substring.py
+++ random_problems/substring.py
@@ -1,6 +1,11 @@
 # Check if a string ends with the given target string
 def substring(string, endstring):
     if string.find(endstring):
-        return string[-len(endstring):]
+        return string[-len(endstring):] == endstring
 
-print substring("Rahul Shelke", "Shelke")
+string = "Rahul Shelke"
+endstring = "Shelke"
+if substring(string, endstring):
+    print("The string {} ends with {}".format(string, endstring))
+else:
+    print("The string {} does not end with {}".format(string, endstring))
