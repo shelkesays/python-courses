e0026ee374a756674fd3d4806ef02bd67fc6bf34
Rahul Shelke <srahul07@gmail.com>
2017-05-18 10:37:47 +0530

    Adding websites visitors

--- pfe/plotting/websites_visitors.py
+++ pfe/plotting/websites_visitors.py
@@ -20,3 +20,25 @@ data_combined.head()
 
 data_combined.plot()
 plt.show()
+
+data = pd.read_csv("operating-systems.csv")
+data.head()
+
+data.columns
+
+data["Value"]
+
+data["Item"]
+
+data.rename(columns = {'Item' : "Operating System"}, inplace=True)
+data.head()
+
+os_new = data[["Operating System", "Value Percent"]]
+
+os_new.set_index("Operating System", inplace=True)
+os_new.head()
+
+explode = (0.1, 0, 0, 0, 0)
+colors_mine = ['yellowgreen', 'gold', 'lightskyblue', 'lightcoral', 'lightcyan']
+os_new[:5].plot(kind="pie", y="Value Percent",autopct='%.2f%%', shadow=True,
+                explode=explode, legend = False, colors = colors_mine)
