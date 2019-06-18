390c17ca280c1f7011d26c1bfefb90258785eedd
Rahul Shelke <srahul07@gmail.com>
2019-06-18 10:18:32 +0530

    Updating

---
+++ courses/lpthw/string_operations.py
@@ -0,0 +1,13 @@
+first = 23
+second = 45
+
+print(first)
+
+print("%d" % first)
+
+print("{}".format(first))
+
+print("{1} + {0}".format(first, second))
+
+print("{val2} + {val1}".format(val1=first, val2=second))
+
